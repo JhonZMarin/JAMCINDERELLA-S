c4cb20c30b7ef013105a95244468496fa0ff5a74
PaulaaGr <pngonzalez@uniboyaca.edu.co>
2025-10-19 16:29:46 -0500

    tiempo

--- Recycling Cinderella's Rats/Assets/Scripts/Personaje.cs
+++ Recycling Cinderella's Rats/Assets/Scripts/Personaje.cs
@@ -27,7 +27,7 @@ public class PlayerController : MonoBehaviour
 
     [Header("Temporizador")]
     public TextMeshProUGUI timerText;
-    private float timeRemaining = 600f;
+    private float timeRemaining = 200f;
     private bool gameEnded = false;
 
     [Header("Audio General")]
