762407d1aaf5a370aed7cd9b2487e0137ee08b45
saraWS <sjwilches@uniboyaca.edu.co>
2025-10-19 02:16:40 -0500

    animacion up down right left

--- Recycling Cinderella's Rats/Assets/Scripts/Personaje.cs
+++ Recycling Cinderella's Rats/Assets/Scripts/Personaje.cs
@@ -2,26 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Personaje : MonoBehaviour
+public class Player : MonoBehaviour
 {
-    [SerializeField] private float velocidad;
+    public float speed = 5f;
 
-    private Rigidbody2D rig;
-    private Animator anim;
+    private Rigidbody2D rb2D;
+    private Vector2 movementInput;
+    private Animator animator;
 
-    private void Awake()
+    void Start()
     {
-        rig = GetComponent<Rigidbody2D>();
-        anim = GetComponent<Animator>();
+        rb2D = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        movementInput.x = Input.GetAxisRaw("Horizontal");
+        movementInput.y = Input.GetAxisRaw("Vertical");
+
+        movementInput = movementInput.normalized;
+
+        animator.SetFloat("Horizontal", movementInput.x);
+        animator.SetFloat("Vertical", movementInput.y);
+        animator.SetFloat("Speed", movementInput.magnitude);
     }
 
     private void FixedUpdate()
     {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
-
-        rig.linearVelocity = new Vector2(horizontal, vertical) * velocidad;
-
-        anim.SetFloat("Caminar", Mathf.Abs(rig.linearVelocity.magnitude));
+        rb2D.linearVelocity = movementInput * speed;
     }
 }
